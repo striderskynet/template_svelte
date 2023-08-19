61cbcd5934d3ad77cd258ff7e0f074bcfdfa8e6a
Jose Ernesto Olazabal <os.nesty@gmail.com>
2023-08-18 20:18:37 -0400

    case

--- src/routes/faq/+page.svelte
+++ src/routes/faq/+page.svelte
@@ -12,7 +12,8 @@
 
 <main class="min-h-screen bg-slate-900 text-base-100 flex flex-col justify-center items-center">
 	<div class="w-[50%] flex flex-col items-center justify-center glass p-5">
-		<!-- <div class="text-xl m-5">Herramientas</div> -->
+		<div class="text-xl mb-2">Numero de Caso</div>
+
 		<div class="flex flex-col relative">
 			<input
 				on:change={handleCase}
@@ -23,6 +24,11 @@
 				<Icon icon="mdi:pound" class="text-xl" />
 			</span>
 		</div>
+		<div class="flex gap-5 mt-5">
+			<a href="https://www.uscis.gov/">www.uscis.gov</a>
+			<a href="https://acis.eoir.justice.gov/es/">acis.eoir.justice.gov/es/</a>
+			<a href=">https://egov.uscis.gov/">egov.uscis.gov</a>
+		</div>
 	</div>
 
 	<div class="mt-10 w-[50%] bg-slate-600 rounded-xl p-10 min-h-[20vh] flex flex-col gap-5">
@@ -30,24 +36,24 @@
 			Esperando.
 		{:then res}
 			{#if res?.CaseStatusResponse.isValid}
-				<div class="text-sm flex gap-2">
-					<span class="text-base">Form:</span>
+				<div class="text-sm flex items-center gap-2">
+					<span class="text-base font-bold">Form:</span>
 					{res.CaseStatusResponse.detailsEs.formNum}
 				</div>
-				<div class="text-sm flex gap-2">
-					<span class="text-base">Form:</span>
+				<div class="text-sm flex items-center gap-2">
+					<span class="text-base font-bold">Tipo:</span>
 					{res.CaseStatusResponse.detailsEs.formTitle}
 				</div>
-				<div class="text-sm flex gap-2">
-					<span class="text-base">Siguiente Paso:</span>
+				<div class="text-sm flex items-center gap-2">
+					<span class="text-base font-bold">Siguiente Paso:</span>
 					{res.CaseStatusResponse.detailsEs.actionCodeText}
 				</div>
-				<div class="text-sm flex gap-2">
-					<span class="text-base">Detalles:</span>
+				<div class="text-sm flex items-center gap-2">
+					<span class="text-base font-bold">Detalles:</span>
 					<span>{@html res.CaseStatusResponse.detailsEs.actionCodeDesc}</span>
 				</div>
 			{:else}
-				Escriba el caso correctamente
+				Escriba el caso correctamente.
 			{/if}
 		{:catch error}
 			{error.message}
